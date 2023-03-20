0a93ad51d31942a920366e4780e776a5aedd9fd4
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2023-03-20 13:16:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ document.querySelector("#main").remove();
 const newHeader = document.createElement('h1')
 newHeader.id = 'victory';
 newHeader.innerHTML = "SHARON is the champion";
+
+document.createElement('h1')
+const divContent = document.body.append('h1')
+h1.innerHTML = "Hello, visitor";
